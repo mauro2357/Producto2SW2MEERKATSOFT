d51add991d221f53eff8703aeaefbdf285688faa
Crisman Carmona <crisman999@outlook.com>
2015-07-31 20:17:08 -0500

    clases

---
+++ Meerkat_0.0/src/main/java/tomaynotificacionpedidos/Mesero.java
@@ -0,0 +1,15 @@
+package tomaynotificacionpedidos;
+
+public class Mesero {
+
+	public void consultarproductos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getMensaje() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
