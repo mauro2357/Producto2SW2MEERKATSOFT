12cf1c376211bb0255eb0a6f4149d80f939b707e
Crisman Carmona <crisman999@outlook.com>
2015-07-31 20:04:45 -0500

    features

---
+++ Meerkat_0.0/src/test/java/tomaynotificacionpedidos/listadodeproductosparameseroStepDefinitions.java
@@ -0,0 +1,27 @@
+package tomaynotificacionpedidos;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class listadodeproductosparameseroStepDefinitions {
+	
+	@Given("^El mesero va a consultar los productos.$")
+	public void El_mesero_va_a_consultar_los_productos() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@When("^No hay productos.$")
+	public void No_hay_productos() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+	@Then("^Notificar que no hay productos para vender.$")
+	public void Notificar_que_no_hay_productos_para_vender() throws Throwable {
+	    // Express the Regexp above with the code you wish you had
+	    throw new PendingException();
+	}
+
+}
