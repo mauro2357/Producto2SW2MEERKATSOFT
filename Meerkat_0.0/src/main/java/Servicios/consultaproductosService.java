c5f8d2ccc5c4b13bc6af81082e57d175647b9960
Crisman Carmona <crisman999@outlook.com>
2015-10-02 13:09:18 -0500

    andriod

--- Meerkat_0.0/src/main/java/Servicios/consultaproductosService.java
+++ Meerkat_0.0/src/main/java/Servicios/consultaproductosService.java
@@ -1,16 +1,43 @@
 package Servicios;
 
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+
+import Negocio.pedido.Producto;
+import Presentacion.MeserosFacade;
+
 
 @Path("consultaproductos")
+@Produces("text/plain")
 public class consultaproductosService {
-	
+    public consultaproductosService() {
+    }
+
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String consultaproductos(){
-		return "Hola";
+	@Produces("text/plain")
+	@Path("consultarproductos/{mesero}")
+	public String consultarproductos(@PathParam("mesero") String mesero) throws Exception { 
+		MeserosFacade meserosFacade = new MeserosFacade();
+		meserosFacade.Consultar_meseros();
+		meserosFacade.Definir_mesero(mesero);
+		ArrayList<Producto> y = meserosFacade.mesero.productos;
+		ArrayList<String> nombresproductos = new ArrayList<String>();
+		for(Producto producto: y){
+			nombresproductos.add(producto.getNombre());
+		}
+		return nombresproductos.toString();
+	}
+
+	@PUT
+	@Consumes("text/plain")
+	public void resourceMethodPUT(String content) { 
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
 	}
 }
