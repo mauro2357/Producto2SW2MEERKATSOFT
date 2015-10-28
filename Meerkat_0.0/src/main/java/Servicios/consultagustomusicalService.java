634cdd4530049f59684f6a73c5a20e5d8bef7e06
Sebastian Eusse <s-e-b-a-s.94@hotmail.com>
2015-10-27 20:07:03 -0500

    services

---
+++ Meerkat_0.0/src/main/java/Servicios/consultagustomusicalService.java
@@ -0,0 +1,41 @@
+package Servicios;
+
+import java.util.ArrayList;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import Negocio.cliente.Cliente;
+import Presentacion.ClientesFacade;
+
+
+@Path("consultagustomusical")
+@Produces("text/plain")
+public class consultagustomusicalService {
+    public consultagustomusicalService() {
+    }
+
+	@GET
+	@Produces("text/plain")
+	@Path("consultargustomusical/")
+	public String consultarproductos() throws Exception { 
+		ClientesFacade clientesFacade = new ClientesFacade();
+		clientesFacade.Consultar_clientes();
+		ArrayList<String> x = new ArrayList<String>();
+		for(Cliente cliente: clientesFacade.listaclientes){
+			x.add(cliente.getNombre()+ cliente.getMusica());
+			
+		}
+		return x.toString();
+	}
+
+	@PUT
+	@Consumes("text/plain")
+	public void resourceMethodPUT(String content) { 
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+}
